10becc6e50e871b0d7439b9cdf249d1c3a35b14f
Alastair Edmonds <Alastair.Edmonds@gmail.com>
2018-07-17 17:18:04 +0100

    6

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,17 @@
 class Oystercard
   attr_reader :balance
 
+  MAX_BALANCE = 90
+
   def initialize(balance = 0.00)
     @balance = balance
   end
 
   def top_up(amount)
+    fail 'ERROR - EXCEEDS MAX. BALANCE' if @balance + amount > MAX_BALANCE
     @balance += amount
   end
+
+
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -10,5 +10,16 @@ describe Oystercard do
     expect(subject.top_up(25)).to eq 25
   end
 
+  it 'raises an error if maximum balance is exceeded' do
+    maximum_balance = Oystercard::MAX_BALANCE
+    subject.top_up(maximum_balance)
+    expect { subject.top_up(1) }.to raise_error "ERROR - EXCEEDS MAX. BALANCE"
+  end
+
+# Q. why does this test not pass?  fail vs. raise_error 
+  # it 'raises an error if max balance is exceeded' do
+  #   subject.top_up(50)
+  #   expect { subject.top_up(41) }.to raise_error 'Exceeds max balance'
+  # end
 
 end
