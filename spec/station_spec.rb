83062fed1169083b9208cbd495101d0702f122c6
Alastair Edmonds <Alastair.Edmonds@gmail.com>
2018-07-18 15:26:45 +0100

    add tests for station name and zone

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,14 +1,18 @@
 require 'station'
 
-
 describe Station do
   subject(:station) { Station.new('Bond Street', 1) }
-  # subject(:station) { described_class.new('Bond Street', 1) }
+# option 2
+  # subject { described_class.new(name: 'Bond Street', zone: 1) }
 
   describe '#initialize' do
     it 'has name' do
-      expect(station.name).to eq 'Bond Street'
-      # expect(subject.name).to eq 'Bond Street'
+      expect(subject.name).to eq 'Bond Street'
+# op 2
+      # expect(subject.name).to eq('Bond Street')
+    end
+    it 'has a zone' do
+      expect(subject.zone).to eq(1)
     end
   end
 
